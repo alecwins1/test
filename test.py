448e6ac7f87bc2d4e4d758d02d13140f90775ee6
alecwins1 <alec-wins@hotmail.co.uk>
2022-01-28 21:03:13 +0000

    Initial Commit

---
+++ test.py
@@ -0,0 +1,12 @@
+"""
+rip
+"""
+
+import math  # pylint: disable=unused-import
+import os  # pylint: disable=unused-import
+import sys  # pylint: disable=unused-import
+
+import requests  # pylint: disable=unused-import
+
+print(sys.version)
+print(sys.executable)
